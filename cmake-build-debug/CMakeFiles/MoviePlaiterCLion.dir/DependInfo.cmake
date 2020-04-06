b3f642518309a2982287a25c8ffe18f441239aa3
John Chittam <john@jchittam.com>
2020-04-05 20:39:03 -0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/MoviePlaiterCLion.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/csuser/Documents/code/A3/MoviePlaiterCLion/Model/Movie.cpp" "/home/csuser/Documents/code/A3/MoviePlaiterCLion/cmake-build-debug/CMakeFiles/MoviePlaiterCLion.dir/Model/Movie.cpp.o"
+  "/home/csuser/Documents/code/A3/MoviePlaiterCLion/Model/ratings.cpp" "/home/csuser/Documents/code/A3/MoviePlaiterCLion/cmake-build-debug/CMakeFiles/MoviePlaiterCLion.dir/Model/ratings.cpp.o"
+  "/home/csuser/Documents/code/A3/MoviePlaiterCLion/Utils/Utils.cpp" "/home/csuser/Documents/code/A3/MoviePlaiterCLion/cmake-build-debug/CMakeFiles/MoviePlaiterCLion.dir/Utils/Utils.cpp.o"
+  "/home/csuser/Documents/code/A3/MoviePlaiterCLion/View/AddMovieWindow.cpp" "/home/csuser/Documents/code/A3/MoviePlaiterCLion/cmake-build-debug/CMakeFiles/MoviePlaiterCLion.dir/View/AddMovieWindow.cpp.o"
+  "/home/csuser/Documents/code/A3/MoviePlaiterCLion/View/DeleteMovieWindow.cpp" "/home/csuser/Documents/code/A3/MoviePlaiterCLion/cmake-build-debug/CMakeFiles/MoviePlaiterCLion.dir/View/DeleteMovieWindow.cpp.o"
+  "/home/csuser/Documents/code/A3/MoviePlaiterCLion/View/MovieLibraryWindow.cpp" "/home/csuser/Documents/code/A3/MoviePlaiterCLion/cmake-build-debug/CMakeFiles/MoviePlaiterCLion.dir/View/MovieLibraryWindow.cpp.o"
+  "/home/csuser/Documents/code/A3/MoviePlaiterCLion/View/OKCancelWindow.cpp" "/home/csuser/Documents/code/A3/MoviePlaiterCLion/cmake-build-debug/CMakeFiles/MoviePlaiterCLion.dir/View/OKCancelWindow.cpp.o"
+  "/home/csuser/Documents/code/A3/MoviePlaiterCLion/main.cpp" "/home/csuser/Documents/code/A3/MoviePlaiterCLion/cmake-build-debug/CMakeFiles/MoviePlaiterCLion.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Model"
+  "../Utils"
+  "../View"
+  "/usr/lib/fltk"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/MoviePlaiterCLion.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MoviePlaiterCLion.dir/Model/Movie.cpp.o"
+  "CMakeFiles/MoviePlaiterCLion.dir/Model/ratings.cpp.o"
+  "CMakeFiles/MoviePlaiterCLion.dir/Utils/Utils.cpp.o"
+  "CMakeFiles/MoviePlaiterCLion.dir/View/AddMovieWindow.cpp.o"
+  "CMakeFiles/MoviePlaiterCLion.dir/View/DeleteMovieWindow.cpp.o"
+  "CMakeFiles/MoviePlaiterCLion.dir/View/MovieLibraryWindow.cpp.o"
+  "CMakeFiles/MoviePlaiterCLion.dir/View/OKCancelWindow.cpp.o"
+  "CMakeFiles/MoviePlaiterCLion.dir/main.cpp.o"
+  "MoviePlaiterCLion"
+  "MoviePlaiterCLion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MoviePlaiterCLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
